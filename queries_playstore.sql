39964df039f60ce503be1335fc51cf6944edf786
Tomomi-K1 <tomomi.music@gmail.com>
2022-09-24 08:46:48 -0700

    compared to the answer

--- queries_playstore.sql
+++ queries_playstore.sql
@@ -16,3 +16,27 @@ BY rating DESC;
 13.playstore=# SELECT SUM(reviews) FROM analytics WHERE reviews IS NOT NULL;
 14.playstore=# SELECT category, COUNT(app_name) FROM analytics  GROUP BY category having COUNT(app_name) > 300;
 15.Playstore=# SELECT app_name, reviews, min_installs, min_installs/reviews AS proportion FROM analytics WHERE min_installs >= 100000 ORDER BY proportion DESC LIMIT 1; 
+
+-- FURTHER STUDY 
+
+-- FS1. Find the name and rating of the top rated apps in each category, 
+-- among apps that have been installed at least 50,000 times.
+SELECT app_name, rating, category FROM analytics
+  WHERE (rating, category) in (
+    SELECT MAX(rating), category FROM analytics
+      WHERE min_installs >= 50000
+      GROUP BY category
+    )
+  ORDER BY category;
+
+-- FS2. Find all the apps that have a name similar to "facebook".
+SELECT * FROM analytics 
+  WHERE app_name ILIKE '%facebook%';
+
+-- FS3. Find all the apps that have more than 1 genre.
+SELECT * FROM analytics 
+  WHERE  array_length(genres, 1) = 2;
+
+-- FS4. Find all the apps that have education as one of their genres.
+SELECT * FROM analytics 
+  WHERE genres @> '{"Education"}';
